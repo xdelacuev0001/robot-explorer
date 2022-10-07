ec8471c3646ba4f676ec2204f124b1be06fe7b46
xdelacuev1 <90628977+xdelacuev1@users.noreply.github.com>
2022-10-06 20:19:43 -0400

    dev class2

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "Xenia";
+  let varname = "";
+  if (name == "Xenia") {
+    varname = (<p> Hey my name is xenia </p>);
+  }
+  const buttonclick = () => {
+    console.log("I clicked the button")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+     <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+      <p> Hey my name is {name}</p>
+      {varname}
+      <button onClick={buttonclick}>I am a button</button>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,5 +33,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
